3ce3881714b61cb161465dbd064ee77049cdc19c
Martincz Gao <martincz@mokeedev.com>
2022-07-07 15:25:06 +0800

    alioth: mokee-ify
    Change-Id: I008d509f5a78c668cbcddb6c2fc0a24e0d1e862c

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/mokee_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_alioth-user \
-    lineage_alioth-userdebug \
-    lineage_alioth-eng
+    mokee_alioth-user \
+    mokee_alioth-userdebug \
+    mokee_alioth-eng
